540a76f776db0a6eb2056ebf068675a89daca9de
atursi508 <atursi@marion-isd.org>
2026-07-22 14:05:59 -0500

    Implement FizzBuzz logic: Fizz for multiples of 3, Buzz for multiples of 5, FizzBuzz for both

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 class FizzBuzz
   def print_numbers
     (1..100).each do |number|
-      puts number
+      output = ""
+      output += "Fizz" if number % 3 == 0
+      output += "Buzz" if number % 5 == 0
+      output = number.to_s if output.empty?
+      puts output
     end
   end
 end
